2a7eaf9213354208b0638a29fdb97b43eaba698a
Ayodele Fagbami <afagbami@gmail.com>
2023-10-03 00:57:54 +0100

    button

--- components/shared/Carousel.tsx
+++ components/shared/Carousel.tsx
@@ -53,8 +53,11 @@ const Carousel = ({ slides }: any) => {
           </div>
         );
       })}
-      <div className="absolute text-center justify-center inset-0 flex items-center  text-white md:text-5xl text-2xl font-bold max-w-4xl">
+      <div className="absolute text-center justify-center inset-0 flex flex-col gap-8 items-center  text-white md:text-5xl text-2xl font-bold max-w-4xl">
         <h1>{slides[current].text}</h1>
+        <button className="bg-primary-green hover:bg-bg-light-green hover:text-primary-green text-white font-bold py-2 px-4 rounded text-sm">
+          LEARN MORE
+        </button>
       </div>
       <div className="absolute bottom-5 left-0 right-0 flex justify-center space-x-2">
         {slides.map((_, index) => (
