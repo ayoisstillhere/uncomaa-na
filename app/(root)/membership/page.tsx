3a18f1554a85a6e5106034a70d724ba3d9d09b4b
Ayodele Fagbami <afagbami@gmail.com>
2024-01-07 14:21:44 -0600

    Update page.tsx

--- app/(root)/membership/page.tsx
+++ app/(root)/membership/page.tsx
@@ -12,7 +12,7 @@ const page = () => {
           Working together to promote and foster the interests of The University
           of Nigeria College of Medicine
         </h6>
-        <div className="lg:grid lg:grid-cols-2 xl:grid-cols-2 gap-4 md:flex md:flex-wrap justify-center lg:px-32 md:px-20 ">
+        {/* <div className="lg:grid lg:grid-cols-2 xl:grid-cols-2 gap-4 md:flex md:flex-wrap justify-center lg:px-32 md:px-20 ">
           {membershipClasses.map((membershipClass, index) => (
             <MemberClassCard
               key={index}
@@ -21,7 +21,17 @@ const page = () => {
               imageSrc={membershipClass.imageSrc}
             />
           ))}
-        </div>
+        </div> */}
+        <div className="flex items-center justify-center mt-4">
+      <div className="bg-white p-8 rounded shadow-md w-96">
+        <h1 className="text-2xl font-bold mb-4">UNCOMAA-USA Alumni Association</h1>
+        <p className="text-gray-600 mb-4">
+          UNCOMAA-USA is a 501-3c organization. Annual dues and any donations made to UNCOMAA-USA are tax deductible.
+        </p>
+        <p className="text-gray-600 mb-4">Please Zelle $200 membership dues, and other charitable funds to:</p>
+        <p className="text-blue-500 font-semibold mb-4">❗️❗️uncomaa@gmail.com❗️❗️</p>
+      </div>
+    </div>
       </div>
     </div>
   );
