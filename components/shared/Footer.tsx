f28c32c2a17356c122e34f257c79e9c1560d74a3
Ayodele Fagbami <afagbami@gmail.com>
2024-01-15 21:43:40 +0100

    link

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -41,36 +41,36 @@ const Footer = () => {
         <div>
           <h6 className="font-medium text-gray-400 max-sm:pt-4">Membership</h6>
           <ul>
-            <li className="py-2 text-sm">Become a member</li>
-            <li className="py-2 text-sm">Membership Forum</li>
-            <li className="py-2 text-sm">Membership Benefits</li>
-            <li className="py-2 text-sm">Renew Membership</li>
+            <a href=""><li className="py-2 text-sm">Become a member</li></a>
+            <a href=""><li className="py-2 text-sm">Membership Forum</li></a>
+            <a href=""><li className="py-2 text-sm">Membership Benefits</li></a>
+            <a href=""><li className="py-2 text-sm">Renew Membership</li></a>
           </ul>
         </div>
         <div>
           <h6 className="font-medium text-gray-400 max-md:pt-4">Support</h6>
           <ul>
-            <li className="py-2 text-sm">Donate</li>
-            <li className="py-2 text-sm">Work with us</li>
-            <li className="py-2 text-sm">Membership Guides</li>
-            <li className="py-2 text-sm">API Status</li>
+            <a href=""><li className="py-2 text-sm">Donate</li></a>
+            <a href=""><li className="py-2 text-sm">Work with us</li></a>
+            <a href=""><li className="py-2 text-sm">Membership Guides</li></a>
+            <a href=""><li className="py-2 text-sm">API Status</li></a>
           </ul>
         </div>
         <div>
           <h6 className="font-medium text-gray-400 max-sm:pt-4">Quick Links</h6>
           <ul>
-            <li className="py-2 text-sm">About</li>
-            <li className="py-2 text-sm">Membership</li>
-            <li className="py-2 text-sm">Jobs</li>
-            <li className="py-2 text-sm">Contribute</li>
+            <a href=""><li className="py-2 text-sm">About</li></a>
+            <a href=""><li className="py-2 text-sm">Membership</li></a>
+            <a href=""><li className="py-2 text-sm">Jobs</li></a>
+            <a href=""><li className="py-2 text-sm">Contribute</li></a>
           </ul>
         </div>
         <div>
           <h6 className="font-medium text-gray-400 max-sm:pt-4">Other Links</h6>
           <ul>
-            <li className="py-2 text-sm">Events</li>
-            <li className="py-2 text-sm">Education</li>
-            <li className="py-2 text-sm">Contact us</li>
+            <a href=""><li className="py-2 text-sm">Events</li></a>
+            <a href=""><li className="py-2 text-sm">Education</li></a>
+            <a href=""><li className="py-2 text-sm">Contact us</li></a>
           </ul>
         </div>
       </div>
